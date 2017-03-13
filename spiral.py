9df7af7038cc46f7d32cc81c10d75a4986221333
tuckershannon <tuckershannon@Tuckers-MacBook-Pro.local>
2017-03-12 22:50:14 -0700

    added black and white background options

--- spiral.py
+++ spiral.py
@@ -1,3 +1,15 @@
+""" Module spiral.py
+    
+    This python program converts images into an Archimedes spiral type art made by varying
+    the width of spiral as it travels away from the center
+
+    Author: Tucker Shannon
+
+
+
+"""
+
+
 
 import numpy
 from math import cos, sin, pi
@@ -17,10 +29,12 @@ def add_photo(img,imagelist):
 	smooth_img = cv2.blur(smooth_img,(3,3))
 	imagelist.append(smooth_img)
 
-def plot_spiral(r,img,parray,loops=8, makeGif=False):
+def plot_spiral(r,img,parray,loops=8, makeGif=False,background=0):
 	theta = 0
-	imagelist = [img.convert(mode="RGB")];
+        fade = 55
+	imagelist = Image.fromarray(img.astype('uint8'))
 	t = 0
+        maxInt = numpy.amax(parray)
 	init_radius = r
 	(x_limit,y_limit) = parray.shape
 	count = 0
@@ -32,20 +46,25 @@ def plot_spiral(r,img,parray,loops=8, makeGif=False):
 		theta += float(1)/(2*r)
 		count = count + 1
 		if t==0:
-			r = [0]
+			radii = [0]
 		else:
-			r = xrange(-t,t+1)
-		for i in r:
+			radii = xrange(int(-t),int(t)+1)
+		for i in radii:
 			r = (init_radius)*theta + i 
 			x = x_limit/2 + int(r*cos(theta))
 			y = y_limit/2 + int(r*sin(theta))
-			if x >0 and x < x_limit and y > 0 and y<y_limit and r>=thickness:
-				img.putpixel((x,y),1)
-				new_thickness=int(parray[x,y])
-				if (new_thickness>t):
-					t = t + 1;
-				if (new_thickness<t):
-					t = t - 1
+			if x >0 and x < x_limit and y > 0 and y<y_limit:
+                            if background == 1:
+                                img[x][y] = [0,0,0]
+                                new_thickness=maxInt-int(parray[x,y])
+                            else: 
+                                img[x][y] = [255,255,255]
+                                new_thickness=(parray[x,y])
+
+		if (new_thickness>t):
+		    t = t + .2;
+		if (new_thickness<t):
+		    t = t - .2
 
 				
 	print count
@@ -60,30 +79,36 @@ def get_photo_array(width,thickness):
     pic = image.resize((sizex,sizey))
     r, g, b = pic.split()
     im = numpy.array(g)
-    im = im/(256/(thickness*pi))
+    thickness = thickness*3
+    im = im/(256/(thickness))
     im = numpy.around(im, decimals=0, out=None)
     photoarray = numpy.zeros([sizex,sizey])
     for num in range(0,numpy.size(im,1)):
                  for num2 in range(0,numpy.size(im,0)):
-                    if int(im[num2,num])> 4:
-                        photoarray[num][num2] = 4
-                    else:
-                        photoarray[num][num2] = int(im[num2,num])
+                    photoarray[num][num2] = int(im[num2,num])
                                  
     return photoarray
 
 if __name__== '__main__':
-	thickness = 2
+	thickness = 3
+        background = 0; #0 for black 1 for white
 	sequence = []
 	photoarray = get_photo_array(2000,thickness)
+        print numpy.amax(photoarray)
 	print photoarray.shape
-	#numpy.set_printoptions(threshold=numpy.nan)
 	IMAGE_SIZE = photoarray.shape
-	img = Image.new('1',IMAGE_SIZE)
-	plot_spiral(thickness,img,photoarray,50,False)
-	smooth_photo(img)
-	#img.save('test.png')
-	print img.size
+        print IMAGE_SIZE[0]
+        IMAGE_SIZE = IMAGE_SIZE + (3,)
+        if background == 1:
+            img = numpy.full(IMAGE_SIZE,255)
+        else:  
+            img = numpy.full(IMAGE_SIZE,0)
+
+	print img[0][0]
+        plot_spiral(thickness,img,photoarray,35,False,background)
+        img = numpy.swapaxes(img,0,1)
+	img = Image.fromarray(img.astype('uint8'))
+        img.save('test2.png')
 
     
 
